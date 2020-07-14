32c1de8bba720bac9c43ceca879bd575cda0bf1b
Strandedd <stranded.equipment@gmail.com>
2020-07-14 15:09:36 +0100

    matlabupdate

--- 3.GNU+OCTAVE/passive_radar/DFT.m
+++
@@ -1,9 +0,0 @@
-function X = Dft(x)
-% If the input x is an array:
-if isvector(x) == 1
-X = fftshift(fft(x))/length(x);
-else
-% x the input x is a matrix:
-X = fftshift(fft(x),1)/ size (x,1);
-end
-endfunction
--- 3.GNU+OCTAVE/passive_radar/IDft.m
+++
@@ -1,9 +0,0 @@
-function x = IDft(X)
-% If the input x is an array:
-if isvector(X) == 1
-x = ifft(ifftshift(X))*length(X);
-else
-% x the input x is a matrix:
-x = ifft(ifftshift(X),1)*size (x,1);
-end
-endfunction
